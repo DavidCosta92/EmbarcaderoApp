dd471bc0842a457247ec9ace71d5b66479bfb1b1
David Costa <89800442+DavidCosta92@users.noreply.github.com>
2024-01-27 18:53:53 -0300

    first commit

---
+++ src/main/java/com/Embarcadero/demo/EmbarcaderoAppApplication.java
@@ -0,0 +1,13 @@
+package com.Embarcadero.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbarcaderoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmbarcaderoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Embarcadero/demo/EmbarcaderoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Embarcadero.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbarcaderoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
