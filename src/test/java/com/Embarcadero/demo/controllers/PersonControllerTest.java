a73a7796093271403f8cd7208a0272c855258496
David Costa <89800442+DavidCosta92@users.noreply.github.com>
2024-04-26 22:08:24 -0300

    test (files controller): add some integration test in files controller

--- src/test/java/com/Embarcadero/demo/controllers/PersonControllerTest.java
+++ src/test/java/com/Embarcadero/demo/controllers/PersonControllerTest.java
@@ -1,25 +1,36 @@
 package com.Embarcadero.demo.controllers;
 
 import com.Embarcadero.demo.auth.UserRepository;
-import com.Embarcadero.demo.auth.entities.AuthResponse;
-import com.Embarcadero.demo.auth.entities.RegisterRequest;
-import com.Embarcadero.demo.auth.entities.User;
+import com.Embarcadero.demo.auth.entities.*;
+import com.Embarcadero.demo.model.dtos.person.PersonReadDto;
+import com.Embarcadero.demo.model.entities.Person;
+import com.Embarcadero.demo.model.repositories.PersonRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwt;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Collection;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -27,7 +38,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class PersonControllerTest {
     // crear usuarios en base de datos..
-    User goodUser = new User().builder()
+    User superAdminUser = new User().builder()
             .username("david1")
             .dni("11111111")
             .email("david1@gmail.com")
@@ -36,6 +47,7 @@ class PersonControllerTest {
             .firstName("david")
             .lastName("costa")
             .password("123456789")
+            .role(Role.SUPER_ADMIN)
             .build();
     User goodUser2 = new User().builder()
             .username("david2")
@@ -47,19 +59,19 @@ class PersonControllerTest {
             .lastName("costa")
             .password("123456789")
             .build();
-    User userPreload1 = new User().builder()
-            .username("aegwsdavidCosta")
+    Person personPreload1 = new Person().builder()
             .dni("35924410")
-            .email("davidcst29921@gmail.com")
+            .notes("davidcst29921@gmail.com")
             .phone("2644i647572")
             .emergencyPhone("26h44647572")
-            .firstName("david")
+            .name("david")
             .lastName("costa")
-            .password("123456789")
+            .address("123456789")
             .build();
 
     final String BASE_URL = "/v1/person/";
-    private String goodUserToken;
+    final String BASE_URL_AUTH = "/v1/auth/";
+    private String adminToken;
     private String goodUser2Token;
 
 
@@ -67,35 +79,65 @@ class PersonControllerTest {
     private WebApplicationContext webApplicationContext;
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    PersonRepository personRepository;
     private MockMvc mockMvc;
     @Autowired
     TestRestTemplate restTemplate;
 
 
     @BeforeAll
-    void initialSetUp(){
-        userRepository.save(userPreload1);
-
-
-
+    void initialSetUp() throws Exception {
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
 
-        // registro de good user
-        RegisterRequest goodUserRegisterRequest = userRegisterRequest(goodUser);
-        ResponseEntity<AuthResponse> responseAuthAdmin = restTemplate.postForEntity(BASE_URL+"register", goodUserRegisterRequest,AuthResponse.class);
-        AuthResponse authResponse =  responseAuthAdmin.getBody();
-        goodUserToken = authResponse.getToken();
+        personRepository.save(personPreload1);
 
+        // register admin
+        RegisterRequest  adminRegisterRequest = userRegisterRequest(superAdminUser);
+        ResponseEntity<AuthResponse> responseAuthAdmin = restTemplate.postForEntity(BASE_URL_AUTH+"register", adminRegisterRequest,AuthResponse.class);
+        AuthResponse authResponse2 =  responseAuthAdmin.getBody();
+        adminToken = authResponse2.getToken();
+
+        /*
         // registro de good user2
         RegisterRequest  goodUser2RegisterRequest = userRegisterRequest(goodUser2);
         ResponseEntity<AuthResponse> responseAuthAdmin2 = restTemplate.postForEntity(BASE_URL+"register", goodUser2RegisterRequest,AuthResponse.class);
         AuthResponse authResponse2 =  responseAuthAdmin2.getBody();
         goodUser2Token = authResponse2.getToken();
+
+         */
     }
 
     @Test
-    @WithMockUser(username = "david") // , roles = {"USER"}
-    void findByDni() {
+    @WithMockUser(username = "david", roles = {"SUPER_ADMIN"}) //, roles = {"SUPER_ADMIN"}
+    void findByDni() throws Exception {
+/*
+todo forma 1 restTemplate
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(adminToken);
+        HttpEntity<PersonReadDto> requestEntity = new HttpEntity<>(null, headers); // null es el body, que al ser un get es null
+        ResponseEntity<PersonReadDto> response = restTemplate.exchange(BASE_URL+ personPreload1.getDni(), HttpMethod.GET, requestEntity, PersonReadDto.class);
+
+        assertThat(response.getBody().getDni()).isEqualTo(personPreload1.getDni());
+        assertThat(response.getBody().getName()).isEqualTo(personPreload1.getName());
+        assertTrue(response.getStatusCode().is2xxSuccessful());
+ */
+
+/*
+todo forma 2 MvcResult
+ */
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(adminToken);
+
+        MvcResult findByDniResult = mockMvc.perform(MockMvcRequestBuilders.get(BASE_URL+personPreload1.getDni())
+                        .headers(headers)
+                // .accept(MediaType.APPLICATION_JSON_VALUE)
+                // .contentType(MediaType.APPLICATION_JSON_VALUE)
+                // .content(mapperStringToJSON(request))
+        ).andReturn();
+
+        // assertTrue(newUserResult.getResponse().getContentAsString().contains("token"));
+
     }
 
     @Test
