968bd21e81690395ea3a021d1e3032eb96d4929b
David Costa <89800442+DavidCosta92@users.noreply.github.com>
2024-05-04 14:05:39 -0300

    feat (jasper reports): add charts to jasper report

--- src/main/java/com/Embarcadero/demo/utils/reports/ShiftReportGenerator.java
+++ src/main/java/com/Embarcadero/demo/utils/reports/ShiftReportGenerator.java
@@ -39,21 +39,6 @@ public class ShiftReportGenerator {
         File mainIcon = ResourceUtils.getFile("classpath:images/mainIcon.png");
         return new FileInputStream(mainIcon);
     }
-    private int getTotalPersonByHour(Shift shift, int hour){
-        List<Record> records = shift.getRecords();
-        int totalPersonByHour = 0;
-        // Crear una instancia de Calendar y establecer la fecha
-        Calendar calendar = Calendar.getInstance();
-
-        for (int i =0; i<records.size(); i++){
-            calendar.setTime(records.get(i).getStartTime());
-            int hours = calendar.get(Calendar.HOUR_OF_DAY);
-            if(hours < hour){
-                totalPersonByHour+= records.get(i).getNumberOfGuests()+1;
-            }
-        }
-        return totalPersonByHour;
-    }
     private void getChartDataBoatTypesByLicences(Shift shift) {
         List<Record> records = shift.getRecords();
         Map<String, Integer> boatTypes = new HashMap<>();
@@ -84,83 +69,71 @@ public class ShiftReportGenerator {
             boats+=1; // agregar estado botes por hora suma 1 por cada registro
             personHoursList.add(new PersonHoursReport(records.get(i).getStartTime(), persons , boats));
         }
+        // CHART DE TIPOS MATRICULAS
+        boatTypeList=new ArrayList<>(); // borrar datos cada vez que pido chart
         boatTypes.forEach((type, amount) ->{
             boatTypeList.add(new BoatTypeReport(type, amount));
         });
         // CHART DE MATRICULAS
+        boatLicenceList = new ArrayList<>(); // borrar datos cada vez que pido chart
         boatLicenceList.add(new BoatTypeLicence("Sin matricula",simpleBoat));
         boatLicenceList.add(new BoatTypeLicence("Matriculadas",registeredBoat));
     }
-
-    private JasperPrint getReport(Shift shift) throws IOException, JRException {
-        Map<String, Object> params = new HashMap<String, Object>();
-        params.put("logoPrincipal", getMainIcon());
-
-        // DIQUE
+    private String getDamName (Shift shift){
         String dam = "";
         if(shift.getDam().name() == "DIQUE_ULLUM") dam="Ullum";
         if(shift.getDam().name() == "DIQUE_PUNTA_NEGRA") dam="Punta Negra";
         if(shift.getDam().name() == "DIQUE_CUESTA_DEL_VIENTO") dam="Cuesta del viento";
-        params.put("dam", dam);
-
-        // FECHA
-        params.put("date", shift.getDate().toString());
-        // ESTADO GUARDIA
+        return dam;
+    }
+    private String getShiftState (Shift shift){
         String shiftState = "Estado: finalizada";
         if(!shift.getClose()) shiftState = "Estado: NO FINALIZADA";
-        params.put("state", shiftState);
-        // STAFF
-        params.put("staff", new JRBeanArrayDataSource(shift.getStaff().toArray()));
-        // RECORDS
-        params.put("totalBoats", shift.getTotalBoats());
-        params.put("totalPersons", shift.getTotalPersons());
-
+        return shiftState;
+    }
+    private List<RecordDtoReport> getReportsDto(Shift shift){
         List<Record> records = shift.getRecords();
         List<RecordDtoReport> reportsDto = new ArrayList<>();
 
-
-        // todo falta => PROBARRRRRR PROBARRRRR
-        // todo falta => PROBARRRRRR PROBARRRRR
         // por cada record, crear un record para jasper, que sean strings y que mande los textos tal cual quiero
         for (int i = 0; i<records.size(); i++){
             Record r = records.get(i);
             String boatType = r.getSimpleBoat() != null ? r.getSimpleBoat().getTypeSimpleBoat().name() : r.getLicense().getRegisteredBoat().getTypeLicencedBoat().name();
             String licenseCode = r.getLicense()!= null ?  r.getLicense().getCode() : "NO";
             reportsDto.add(new RecordDtoReport().builder()
-                            .id(r.getId())
-                            .startTime(r.getStartTime())
-                            .endTime(r.getEndTime())
-                            .recordState(r.getRecordState().name())
-                            .licenseCode(licenseCode)
-                            .boatType(boatType)
-                            .personDni(r.getPerson().getDni())
-                            .personPhone(r.getPerson().getPhone())
-                            .numberOfGuests(r.getNumberOfGuests())
-                            .car(r.getCar())
-                            .build());
+                    .id(r.getId())
+                    .startTime(r.getStartTime())
+                    .endTime(r.getEndTime())
+                    .recordState(r.getRecordState().name())
+                    .licenseCode(licenseCode)
+                    .boatType(boatType)
+                    .personDni(r.getPerson().getDni())
+                    .personPhone(r.getPerson().getPhone())
+                    .numberOfGuests(r.getNumberOfGuests())
+                    .car(r.getCar())
+                    .build());
         }
-        params.put("records", new JRBeanCollectionDataSource(reportsDto));
-
-        // todo falta => PROBARRRRRR PROBARRRRR
-        // todo falta => PROBARRRRRR PROBARRRRR
-
-
-
-
-        // Obtener datos para graficos
-        getChartDataBoatTypesByLicences(shift);
+        return reportsDto;
+    }
+    private JasperPrint getReport(Shift shift) throws IOException, JRException {
+        Map<String, Object> params = new HashMap<String, Object>();
+        // HEADER
+        params.put("logoPrincipal", getMainIcon());
+        // SHIFT
+        params.put("dam", getDamName(shift));
+        params.put("date", shift.getDate().toString());
+        params.put("state", getShiftState(shift));
+        params.put("shiftNotes","Notas: "+ shift.getNotes());
+        // STAFF
+        params.put("staff", new JRBeanArrayDataSource(shift.getStaff().toArray()));
+        // RECORDS
+        params.put("totalBoats", shift.getTotalBoats());
+        params.put("totalPersons", shift.getTotalPersons());
+        params.put("records", new JRBeanCollectionDataSource(getReportsDto(shift)));
+        getChartDataBoatTypesByLicences(shift); // setea datos de boatTypeList, de boatLicenceList y  de personHoursList
         params.put("dataSetBoatTypes",new JRBeanCollectionDataSource(boatTypeList));
         params.put("dataSetBoatLicence",new JRBeanCollectionDataSource(boatLicenceList));
-
-        // - Total personas por hora Personas + boat hours
         params.put("dataSetPersonHours",new JRBeanCollectionDataSource(personHoursList));
-
-        // params.put("dataSetBoats",new JRBeanCollectionDataSource(boatsHoursList));
-
-
-
-        // TODO PENDIENTE- Total boat por hora
-        // params.put("dataSetTotalBoatHours",new JRBeanCollectionDataSource(totalBoatList));
 
         try {
             InputStream jrxmlStream = getClass().getResourceAsStream("/Shift_resume_A4.jrxml");
